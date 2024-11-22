a2025b533986f8c0d514ae4e9eb0c071cc4cdacc
Sai Sreenadh Chilukuri <66728798+UrTechTips@users.noreply.github.com>
2024-11-22 18:34:38 +0530

    Update view-user.js

--- view-user.js
+++ view-user.js
@@ -1,6 +1,6 @@
 window.onload = async () => {
   try {
-    const response = await fetch('https://ai-ds-backend-6tnt.onrender.com');  // Adjust URL based on your API
+    const response = await fetch('https://ai-ds-backend-6tnt.onrender.com/users');  // Adjust URL based on your API
     const users = await response.json();
 
     if (response.ok) {
